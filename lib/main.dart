6dcdfd48fde6e99d5fe817e41bd1089c122721af
your.user.name <38698778+Rakibul25@users.noreply.github.com>
2021-04-23 02:18:41 +0600

    yaml done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,72 @@
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
-import 'homepage.dart';
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await DatabaseHelper.initDb();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: SplashScress(),
+    );
+  }
+}
+
+class SplashScress extends StatefulWidget {
+  @override
+  _SplashScressState createState() => _SplashScressState();
+}
+
+class _SplashScressState extends State<SplashScress> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () async {
+      Get.to(LoginPage());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.white),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  alignment: Alignment.center,
+                  height: 150,
+                  width: 150,
+                  child: Icon(
+                    Icons.ac_unit,
+                    size: 50,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
