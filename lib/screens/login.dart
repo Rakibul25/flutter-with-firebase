9a3f3858f48a40faa06a400306c6cf56c494d85f
your.user.name <38698778+Rakibul25@users.noreply.github.com>
2021-04-24 00:18:10 +0600

    ready to run

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'home.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -6,8 +9,71 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String _email, _password;
+  final auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(hintText: 'Email'),
+              onChanged: (value) {
+                setState(() {
+                  _email = value.trim();
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(hintText: 'Password'),
+              onChanged: (value) {
+                setState(() {
+                  _password = value.trim();
+                });
+              },
+            ),
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text('Signin'),
+                onPressed: () {
+                  auth
+                      .signInWithEmailAndPassword(
+                          email: _email, password: _password)
+                      .then((_) {
+                    Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  });
+                  print("Working");
+                }),
+            RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text('Signup'),
+              onPressed: () {
+                auth
+                    .createUserWithEmailAndPassword(
+                        email: _email, password: _password)
+                    .then((_) {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                });
+              },
+            )
+          ])
+        ],
+      ),
+    );
   }
 }
